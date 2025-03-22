043859dc08ece7bc147886cefc8600282046854a
Kirill Medvedev <repkomail@gmail.com>
2025-03-22 17:44:52 +0200

    feat: solve tasks

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -188,8 +188,19 @@ function formatDate(date) {
  * 12, 2023 => 10
  * 1, 2024 => 8
  */
-function getCountWeekendsInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountWeekendsInMonth(month, year) {
+  let count = 0;
+
+  const daysInMonth = new Date(year, month, 0).getDate();
+  for (let day = 1; day <= daysInMonth; day += 1) {
+    const currentDate = new Date(year, month - 1, day);
+
+    if (currentDate.getDay() === 6 || currentDate.getDay() === 0) {
+      count += 1;
+    }
+  }
+
+  return count;
 }
 
 /**
@@ -205,8 +216,19 @@ function getCountWeekendsInMonth(/* month, year */) {
  * Date(2024, 0, 31) => 5
  * Date(2024, 1, 23) => 8
  */
-function getWeekNumberByDate(/* date */) {
-  throw new Error('Not implemented');
+function getWeekNumberByDate(date) {
+  const copiedDate = new Date(date);
+  copiedDate.setHours(0, 0, 0, 0);
+
+  copiedDate.setDate(copiedDate.getDate() + 4 - (copiedDate.getDay() || 7));
+
+  const yearStart = new Date(copiedDate.getFullYear(), 0, 1);
+
+  const diffInTime = copiedDate.getTime() - yearStart.getTime();
+
+  const weekNumber = Math.ceil((diffInTime / (1000 * 60 * 60 * 24) + 1) / 7);
+
+  return weekNumber;
 }
 
 /**
